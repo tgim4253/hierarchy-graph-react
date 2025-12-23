01d9c9366173424851283feb54210b012458f9f2
tgim4253 <167991128+tgim4253@users.noreply.github.com>
2025-12-23 20:00:20 +0900

    Refac/remove chess tree (#5)
    * feat: basic graph logic
    
    * feat: pan zoom
    
    * refac: remove chess tree

--- apps/playground/vite.config.ts
+++ apps/playground/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath } from 'url';
 
-const chessTreeSrc = fileURLToPath(new URL('../../packages/chess-tree-react/src', import.meta.url));
 const hierarchySrc = fileURLToPath(
   new URL('../../packages/hierarchy-graph-react/src', import.meta.url),
 );
@@ -15,7 +14,6 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      'chess-tree-react': chessTreeSrc,
       'hierarchy-graph-react': hierarchySrc,
     },
   },
